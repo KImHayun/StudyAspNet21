948924f0f10d08649666e94eb37d425725abc38b
KimHayun <95-ha@naver.com>
2021-04-19 17:12:56 +0900

    4월19일

---
+++ MvcSolutions/DbWebApp/Migrations/20210419032333_InitialCommit.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DbWebApp.Migrations
+{
+    public partial class InitialCommit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    RoomId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.RoomId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "speakers",
+                columns: table => new
+                {
+                    SpeakerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RoomId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_speakers", x => x.SpeakerId);
+                    table.ForeignKey(
+                        name: "FK_speakers_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "RoomId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_speakers_RoomId",
+                table: "speakers",
+                column: "RoomId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "speakers");
+
+            migrationBuilder.DropTable(
+                name: "Rooms");
+        }
+    }
+}
---
+++ MvcSolutions/MvcWebLab.Database/dbo/Tables/speakers.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[speakers] (
+    [SpeakerId] INT            IDENTITY (1, 1) NOT NULL,
+    [Name]      NVARCHAR (MAX) NOT NULL,
+    [RoomId]    INT            NOT NULL,
+    CONSTRAINT [PK_speakers] PRIMARY KEY CLUSTERED ([SpeakerId] ASC),
+    CONSTRAINT [FK_speakers_Rooms_RoomId] FOREIGN KEY ([RoomId]) REFERENCES [dbo].[Rooms] ([RoomId]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_speakers_RoomId]
+    ON [dbo].[speakers]([RoomId] ASC);
+
